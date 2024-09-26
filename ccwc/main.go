5de61f7661cc6b19036bd4f86404c90985926c5d
Enrico Zoboli <83153234+e-zoob@users.noreply.github.com>
2024-09-26 11:35:09 +0200

    Add word count (#6)
    * add conunt lines func
    
    * add-w

--- ccwc/main.go
+++ ccwc/main.go
@@ -18,6 +18,10 @@ func main() {
 	case "-l":
 		lines := lib.CountLines(path)
 		fmt.Printf("%d %s", lines, path)
+
+	case "-w":
+		words := lib.CountWords(path)
+		fmt.Printf("%d %s", words, path)
 	}
 
 }
